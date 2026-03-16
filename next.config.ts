690e88830ef80b4d1c0fa35f34abc48b251acae5
Dhanush-hub-alt <dhanush80729@gmail.com>
2026-03-16 21:31:18 +0530

    Updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Portfolio", 
   devIndicators: false,
   images: {
     unoptimized: true,
